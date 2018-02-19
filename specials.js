345f86abed8832100fadc4d0e1ff54d7ba8108d4
rwj81 <willjoaquin@yahoo.com>
2018-02-19 15:38:09 -0500

    added style to jq alerts

--- specials.js
+++ specials.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 $("header").one("mouseleave", function(){
-  alert('Be sure to complete our new member sign up to enroll in the Cut Club today!');
+  swal('Be sure to complete our new member sign up to enroll in the Cut Club today!');
 });
 });
 
@@ -12,11 +12,11 @@ const name = document.getElementById('name');
 
 $(document).ready(function(){
 $("button").click(function(){
-  alert('Thank you ' + name.value + ' for signing up.  Be on the lookout for an introductory email soon!');
-});
-});
+  swal('Thank you ' + name.value + ' for signing up.  Be on the lookout for an introductory email soon!');
+  });
+  });
 
-//Next need a way to stop first alert function so it doesn't post after submit button clears.  .one has some benefit but isn't perfect
+//Sweetalert (swal) can be replaced with "alert" for browser-stryled boxes.
 
 
 //button audio functions     https://youtu.be/VlwSz2dXK_8
